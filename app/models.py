38555779a7f9e9506863c5a52a6b2176cfda9cf5
Jakub Oskera <iam@jakuboskera.dev>
2021-10-19 20:52:56 +0200

    fix: Enable multiline comments (#3)
    If user submitted multiline comment it was generated as single line comment.
    
    Closes #2

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class EntriesModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
-    comment = db.Column(db.String(), nullable=False)
+    comment = db.Column(db.Text(), nullable=False)
     created = db.Column(db.DateTime(), nullable=False)
 
     def __init__(self, name, comment):
