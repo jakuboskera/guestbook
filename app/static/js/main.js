311595e9a7d03a3a1ccd75c6f776e886f8a2d2bf
Jakub Oskera <iam@jakuboskera.dev>
2022-01-19 21:12:36 +0100

    feat: implement dark mode

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,6 +1,42 @@
-document.getElementById('anonymous').onchange = function() {
-    document.getElementById('name').disabled = this.checked;
-};
+if (document.getElementById('anonymous')) {
+    document.getElementById('anonymous').onchange = function() {
+        var name = document.getElementById('name');
+        name.disabled = this.checked;
+        name.value = "";
+    }
+}
+
+(function() {
+    var onpageLoad = localStorage.getItem("theme") || "";
+    var body = document.body.classList;
+
+    if (onpageLoad == "bootstrap-dark") {
+        body.remove("bootstrap");
+        body.add("bootstrap-dark")
+        document.getElementById('toggle-dark-mode').checked = true;
+    }
+})();
+
+
+function SwapThemes() {
+    console.log("hi");
+    var body = document.body.classList;
+
+    if (body.contains("bootstrap")) {
+        body.remove("bootstrap");
+        body.add("bootstrap-dark");
+    } else {
+        body.remove("bootstrap-dark");
+        body.add("bootstrap");
+    }
+
+    var theme = localStorage.getItem("theme");
+    if (theme && theme === "bootstrap-dark") {
+        localStorage.setItem("theme", "");
+    } else {
+        localStorage.setItem("theme", "bootstrap-dark");
+    }
+}
 
 new FgEmojiPicker({
     trigger: ['#emoji'],
